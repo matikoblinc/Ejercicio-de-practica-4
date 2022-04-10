23cbe10c03fb6f78070560793c779a37ac91fc33
matikoblinc <78440014+matikoblinc@users.noreply.github.com>
2022-04-10 14:47:40 -0300

    Exercise 9 finished

--- Assets/Scripts/Ej09.cs
+++ Assets/Scripts/Ej09.cs
@@ -4,10 +4,36 @@ using UnityEngine;
 
 public class Ej09 : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public string nombre1;
+    public string nombre2;
+    public string nombre3;
+
+    public float capital1;
+    public float capital2;
+    public float capital3;
+
+    float porcentaje1;
+    float porcentaje2;
+    float porcentaje3;
+
+    float montoTotal;
+    float multi = 100;
+
+
     void Start()
     {
-        
+        montoTotal = capital1 + capital2 + capital3;
+
+        porcentaje1 = capital1 / montoTotal * 100;
+        porcentaje2 = capital2 / montoTotal * 100;
+        porcentaje3 = capital3 / montoTotal * 100;
+
+        Debug.Log("Nombre: " + nombre1 + ", capital aportado: $" + capital1 + ", Porcentaje del capital: %" + porcentaje1 +
+            "Monto total aportado: $" + montoTotal);
+        Debug.Log("Nombre: " + nombre2 + ", capital aportado: $" + capital2 + ", Porcentaje del capital: %" + porcentaje2 +
+            "Monto total aportado: $" + montoTotal);
+        Debug.Log("Nombre: " + nombre3 + ", capital aportado: $" + capital3 + ", Porcentaje del capital: %" + porcentaje3 +
+            "Monto total aportado: $" + montoTotal);
     }
 
     // Update is called once per frame
@@ -16,3 +42,8 @@ public class Ej09 : MonoBehaviour
         
     }
 }
+//9. Realizá un programa que resuelva el siguiente problema:  
+//Tres personas aportan diferente capital a una sociedad. Se desea saber qué porcentaje del  total aportó cada una
+//(indicando nombre y porcentaje) y cuál es el monto del total aportado  por las tres.  
+//Ingresar por Inspector el ingreso del capital aportado por cada una de las personas y luego  mostrar lo pedido en el siguiente formato:  
+//Nombre: capital aportado: $ … , Porcentaje del capital: % .... , Monto total aportado: $ …. 
