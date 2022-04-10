a3a57b4683a55a3894f81771bc882c04c14b08d1
matikoblinc <78440014+matikoblinc@users.noreply.github.com>
2022-04-10 14:09:38 -0300

    Exercise 2 finished

--- Assets/Scripts/Ej02.cs
+++ Assets/Scripts/Ej02.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class Ej02 : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int num1;
+    public int num2;
+    int suma;
+    int producto;
+
     void Start()
     {
-        
+        suma = num1 + num2;
+        Debug.Log("el resultado de la suma es: " + suma);
+
+        producto = num1 * num2;
+        Debug.Log("El resultado del producto es: " + producto);
     }
 
     // Update is called once per frame
@@ -16,3 +24,7 @@ public class Ej02 : MonoBehaviour
         
     }
 }
+//Realizá un programa que, siendo num1 y num2 variables enteras ingresadas por el susario,
+//realice la operación correspondiente y muestre el resultado en pantalla: 
+//a.Suma
+//b.Producto
